1423d41df64db976ce9dd356899fd6d4a29bffdb
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-10-23 22:19:48 -0500

    Removed Junk

--- app/Groomy/Groomy/obj/Debug/net8.0-windows/Groomy.AssemblyInfo.cs
+++ app/Groomy/Groomy/obj/Debug/net8.0-windows/Groomy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Groomy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e34b363a34227c5fd8846c629e66c085ba6be93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169874f8fcf37b39b2d7bd97c56f9e778255855a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Groomy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Groomy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
