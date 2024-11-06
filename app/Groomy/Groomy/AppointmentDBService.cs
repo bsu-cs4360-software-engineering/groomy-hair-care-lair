bd69f8e6d5710f439cf83c40670620f09e7aeabb
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-11-06 17:09:18 -0600

    Created AppointmentDBService

---
+++ app/Groomy/Groomy/AppointmentDBService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomy
+{
+    internal class AppointmentDBService
+    {
+        DatabaseManager _DatabaseManager;
+        public AppointmentDBService(DatabaseManager dbm)
+        {
+            _DatabaseManager = dbm;
+        }
+        public void CreateAppointment(Appointment appointment)
+        {
+            _DatabaseManager.AddObjectsToDB(appointment);
+        }
+        public Dictionary<string, object> ReadAppointmentData(string appointmentID)
+        {
+            return _DatabaseManager.LoadObjectFromDB(appointmentID, Appointment.FilePaths["AppointmentData"]);
+        }
+        public void DeleteAppointment(string appointmentID)
+        {
+            _DatabaseManager.RemoveObjectFromDB(appointmentID, Appointment.FilePaths["AppointmentData"]);
+        }
+        public void SoftDeleteAppointment(string appointmentID)
+        {
+            _DatabaseManager.SoftDeleteObjectInDB(appointmentID, Appointment.FilePaths["AppointmentData"]);
+        }
+    }
+}
