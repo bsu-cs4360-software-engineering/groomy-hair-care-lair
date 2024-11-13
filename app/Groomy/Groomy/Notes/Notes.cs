f119cd043efa1d3c0241aff438e044afd0486409
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-11-12 19:58:15 -0600

    Implemented Notes + customer/appointment relationships

---
+++ app/Groomy/Groomy/Notes/Notes.cs
@@ -0,0 +1,52 @@
+﻿using Groomy.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomy.Notes
+{
+    internal class Notes : IGenericObject
+    {
+        public string noteID;
+        public string title;
+        public string payload;
+        public string createDate;
+
+        public Dictionary<string, string> FilePaths = new Dictionary<string, string>
+        {
+            { "NotesData", "notes.json" }
+        };
+        public Notes(string t, string p, string cd, string nID)
+        {
+            this.noteID = nID;
+            this.title = t;
+            this.payload = p;
+            this.createDate = cd;
+        }
+        public Notes(string t, string p, string cd) : this(t, p, cd, Helpers.RandomSHA256Hash())
+        {
+        }
+        public Dictionary< string, Dictionary<string, string>> GetFields()
+        {
+            var temp = new Dictionary<string, Dictionary<string, string>>();
+            temp["NotesData"] = new Dictionary<string, string>
+                {
+                    { "NoteID", noteID.ToString() },
+                    { "Title", title.ToString() },
+                    { "Payload", payload.ToString() },
+                    { "CreateDate", createDate.ToString() }
+                };
+            return temp;
+        }
+        public string GetKey()
+        {
+            return noteID;
+        }
+        public Dictionary<string, string> GetDBFilePaths()
+        {
+            return FilePaths;
+        }
+    }
+}
---
+++ app/Groomy/Groomy/Relationships/AppointmentNotesRelationship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomy.Relationships
+{
+    internal class AppointmentNotesRelationship
+    {
+        string appointmentID;
+        string noteID;
+        public static string relationshipFilePath = "appointments_notes.json";
+        public AppointmentNotesRelationship(string aID, string nID)
+        {
+            appointmentID = aID;
+            noteID = nID;
+        }
+        public string GetFilePath()
+        {
+            return relationshipFilePath;
+        }
+        public Dictionary<string, string> GetIDs()
+        {
+            var ids = new Dictionary<string, string>();
+            ids.Add("appointmentID", appointmentID);
+            ids.Add("noteID", noteID);
+            return ids;
+        }
+    }
+}
