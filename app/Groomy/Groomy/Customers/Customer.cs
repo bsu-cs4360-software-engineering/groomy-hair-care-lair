7be9ee7d7c90825a1b83a31406a51c97acb30167
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-11-19 20:16:13 -0600

    updated customer class to randomly generate customerID and added new customer constructor

--- app/Groomy/Groomy/Customers/Customer.cs
+++ app/Groomy/Groomy/Customers/Customer.cs
@@ -13,19 +13,23 @@
         private string email;
         private string phoneNumber;
         private string address;
-        public string customerID => Helpers.GenerateSHA256Hash(email);
+        public string customerID;
 
         public static Dictionary<string, string> FilePaths = new Dictionary<string, string>
         {
             { "CustomerData", "customers.json" }
         };
-        public Customer(string fName, string lName, string eMail, string pNumber, string addr)
+        public Customer(string f, string l, string e, string p, string a) : this(f, l, e, p, a, Helpers.RandomSHA256Hash())
+        {
+        }
+        public Customer(string fName, string lName, string eMail, string pNumber, string addr, string cID)
         {
             firstName = fName;
             lastName = lName;
             email = eMail;
             phoneNumber = pNumber;
             address = addr;
+            customerID = cID;
         }
         public Dictionary<string, Dictionary<string, string>> GetFields()
         {
