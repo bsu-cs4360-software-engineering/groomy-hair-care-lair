41d193681414b4d7920e2b244d68f8776092464c
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-11-21 14:07:34 -0600

    Created Service class and ServiceDBService class

---
+++ app/Groomy/Groomy/Services/Service.cs
@@ -0,0 +1,51 @@
+﻿using Groomy.Customers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomy.Services
+{
+    internal class Service : IGenericObject
+    {
+        public string serviceID;
+        public string serviceName;
+        public string serviceDescription;
+        public string servicePrice;
+        public static Dictionary<string, string> FilePaths = new Dictionary<string, string>
+        {
+            { "ServiceData", "services.json" }
+        };
+        public Service(string sn, string sd, string sp, string sID)
+        {
+            this.serviceID = sID;
+            this.serviceName = sn;
+            this.serviceDescription = sd;
+            this.servicePrice = sp;
+        }
+        public Service(string sn, string sd, string sp) : this(sn, sd, sp, Helpers.RandomSHA256Hash())
+        {
+        }
+        public Dictionary<string, Dictionary<string, string>> GetFields()
+        {
+            var temp = new Dictionary<string, Dictionary<string, string>>();
+            temp["ServiceData"] = new Dictionary<string, string>
+                {
+                    { "ServiceID", serviceID.ToString() },
+                    { "ServiceName", serviceName.ToString() },
+                    { "ServiceDescription", serviceDescription.ToString() },
+                    { "ServicePrice", servicePrice.ToString() }
+                };
+            return temp;
+        }
+        public string GetKey()
+        {
+            return serviceID;
+        }
+        public Dictionary<string, string> GetDBFilePaths()
+        {
+            return FilePaths;
+        }
+    }
+}
---
+++ app/Groomy/Groomy/Services/ServiceDBService.cs
@@ -0,0 +1,41 @@
+﻿using Groomy.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomy.Services
+{
+    internal class ServiceDBService
+    {
+        private ManagerSingleton ms;
+        public ServiceDBService(ManagerSingleton ms)
+        {
+            this.ms = ms;
+        }
+        public void CreateService(Service service)
+        {
+            ms.dbm.AddObjectsToDB(service);
+        }
+        public Dictionary<string, string> ReadServiceData(string serviceID)
+        {
+            return ms.dbm.LoadJsonFromDB(serviceID, Service.FilePaths["ServiceData"]);
+        }
+        public void UpdateServiceData(Service service)
+        {
+            var serviceID = service.GetKey();
+            var serviceData = service.GetFields()["ServiceData"];
+
+            ms.dbm.UpdateObjectInDB(serviceID, serviceData, Service.FilePaths["ServiceData"]);
+        }
+        public void DeleteService(string serviceID)
+        {
+            ms.dbm.RemoveObjectFromDB(serviceID, Service.FilePaths["ServiceData"]);
+        }
+        public void SoftDeleteService(string serviceID)
+        {
+            ms.dbm.SoftDeleteObjectInDB(serviceID, Service.FilePaths["ServiceData"]);
+        }
+    }
+}
