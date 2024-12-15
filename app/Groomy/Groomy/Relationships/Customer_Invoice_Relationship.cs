df2235f52483713a189f8720b905ba05f1ffe13f
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-12-15 15:20:24 -0600

    Added customer invoice, and invoice detail relationships

---
+++ app/Groomy/Groomy/Relationships/Customer_Invoice_Relationship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groomy.Relationships
+{
+    internal class Customer_Invoice_Relationship : IRelationship
+    {
+        string customerID;
+        string invoiceID;
+        public static string relationshipFilePath = "customers_invoices.json";
+        public Customer_Invoice_Relationship(string cID, string iID)
+        {
+            this.customerID = cID;
+            this.invoiceID = iID;
+        }
+        public string GetFilePath()
+        {
+            return relationshipFilePath;
+        }
+        public Dictionary<string, string> GetIDs()
+        {
+            var ids = new Dictionary<string, string>();
+            ids.Add("customerID", customerID);
+            ids.Add("invoiceID", invoiceID);
+            return ids;
+        }
+    }
+}
