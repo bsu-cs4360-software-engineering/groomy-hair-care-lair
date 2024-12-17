a3845eff234aecc24dfc7cf7a05db5f84617401c
rlanglie <166043486+rlanglie@users.noreply.github.com>
2024-12-17 01:07:21 -0600

    Finished final NotesDBService test

--- app/Groomy/GroomyTests/Notes/NotesDBServiceTests.cs
+++ app/Groomy/GroomyTests/Notes/NotesDBServiceTests.cs
@@ -705,41 +705,29 @@ namespace Groomy.Notes.Tests
         [TestMethod()]
         public void SoftDeleteCustomerNotesTest()
         {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void SoftDeleteAppointmentNotesTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void SoftDeleteServiceNotesTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void SoftDeleteInvoiceNotesTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void SoftDeleteNoteTest()
-        {
-            //Arrange
+            //Assign
             var mockFS = new Mock<IFileService>();
             var dbm = new DatabaseManager(mockFS.Object);
             var ua = new UserAuth();
             var dbrs = new DBRelationshipService(dbm, ua);
             var ndbs = new NotesDBService(dbm, dbrs);
 
+            var customerID = "testID";
+            var notesID = "testNoteID";
+
+            var customers_notes_relationshipsFilepath = "customers_notes.json";
+            var initialCustomersNotesRelationships = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"customerID", customerID},
+                    {"noteID", notesID}
+                }
+            };
+
             var noteTitle = "Test Note";
             var notePayload = "This is a test note.";
             var noteCreationDate = DateTime.Now.ToString();
-            var notesID = "testID";
 
             var notesDBFilepath = "notes.json";
             var initialNotesDB = new List<Dictionary<string, string>>
@@ -753,8 +741,287 @@ namespace Groomy.Notes.Tests
                 }
             };
 
+            mockFS.Setup(fs => fs.ReadAllText(customers_notes_relationshipsFilepath)).Returns(JsonSerializer.Serialize(initialCustomersNotesRelationships));
+            mockFS.Setup(fs => fs.Exists(customers_notes_relationshipsFilepath)).Returns(true);
 
-            var primaryID = "testID";
+            mockFS.Setup(fs => fs.ReadAllText(notesDBFilepath)).Returns(JsonSerializer.Serialize(initialNotesDB));
+            mockFS.Setup(fs => fs.Exists(notesDBFilepath)).Returns(true);
+
+            string retrievedCustomersNotesRelationships = null;
+            mockFS.Setup(fs => fs.WriteAllText(customers_notes_relationshipsFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedCustomersNotesRelationships = data);
+
+            string retrievedNotesDB = null;
+            mockFS.Setup(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedNotesDB = data);
+
+            //Act
+            ndbs.SoftDeleteCustomerNotes(notesID);
+
+            //Assert
+            Assert.IsNotNull(retrievedCustomersNotesRelationships);
+            //deserialize the json string and check if the relationship was soft deleted
+            var relationships = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedCustomersNotesRelationships);
+            Assert.AreEqual(1, relationships.Count);
+            Assert.AreEqual(customerID, relationships[0]["customerID"]);
+            Assert.AreEqual(notesID, relationships[0]["noteID"]);
+            Assert.AreEqual("true", relationships[0]["IsDeleted"]);
+
+            Assert.IsNotNull(retrievedNotesDB);
+            //deserialize the json string and check if the note was soft deleted
+            var notes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedNotesDB);
+            Assert.AreEqual(1, notes.Count);
+            Assert.AreEqual(noteTitle, notes[0]["Title"]);
+            Assert.AreEqual(notePayload, notes[0]["Payload"]);
+            Assert.AreEqual(noteCreationDate, notes[0]["CreateDate"]);
+            Assert.AreEqual(notesID, notes[0]["NoteID"]);
+            Assert.AreEqual("true", notes[0]["IsDeleted"]);
+        }
+
+        [TestMethod()]
+        public void SoftDeleteAppointmentNotesTest()
+        {
+            //Assign
+            var mockFS = new Mock<IFileService>();
+            var dbm = new DatabaseManager(mockFS.Object);
+            var ua = new UserAuth();
+            var dbrs = new DBRelationshipService(dbm, ua);
+            var ndbs = new NotesDBService(dbm, dbrs);
+
+            var appointmentID = "testID";
+            var notesID = "testNoteID";
+
+            var appointments_notes_relationshipsFilepath = "appointments_notes.json";
+            var initialAppointmentsNotesRelationships = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"appointmentID", appointmentID},
+                    {"noteID", notesID}
+                }
+            };
+
+            var noteTitle = "Test Note";
+            var notePayload = "This is a test note.";
+            var noteCreationDate = DateTime.Now.ToString();
+
+            var notesDBFilepath = "notes.json";
+            var initialNotesDB = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"Title", noteTitle},
+                    {"Payload", notePayload},
+                    {"CreateDate", noteCreationDate},
+                    {"NoteID", notesID}
+                }
+            };
+
+            mockFS.Setup(fs => fs.ReadAllText(appointments_notes_relationshipsFilepath)).Returns(JsonSerializer.Serialize(initialAppointmentsNotesRelationships));
+            mockFS.Setup(fs => fs.Exists(appointments_notes_relationshipsFilepath)).Returns(true);
+
+            mockFS.Setup(fs => fs.ReadAllText(notesDBFilepath)).Returns(JsonSerializer.Serialize(initialNotesDB));
+            mockFS.Setup(fs => fs.Exists(notesDBFilepath)).Returns(true);
+
+            string retrievedAppointmentsNotesRelationships = null;
+            mockFS.Setup(fs => fs.WriteAllText(appointments_notes_relationshipsFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedAppointmentsNotesRelationships = data);
+
+            string retrievedNotesDB = null;
+            mockFS.Setup(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedNotesDB = data);
+
+            //Act
+            ndbs.SoftDeleteAppointmentNotes(notesID);
+
+            //Assert
+            Assert.IsNotNull(retrievedAppointmentsNotesRelationships);
+
+            //deserialize the json string and check if the relationship was soft deleted
+            var relationships = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedAppointmentsNotesRelationships);
+            Assert.AreEqual(1, relationships.Count);
+            Assert.AreEqual(appointmentID, relationships[0]["appointmentID"]);
+            Assert.AreEqual(notesID, relationships[0]["noteID"]);
+            Assert.AreEqual("true", relationships[0]["IsDeleted"]);
+
+            Assert.IsNotNull(retrievedNotesDB);
+            //deserialize the json string and check if the note was soft deleted
+            var notes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedNotesDB);
+            Assert.AreEqual(1, notes.Count);
+            Assert.AreEqual(noteTitle, notes[0]["Title"]);
+            Assert.AreEqual(notePayload, notes[0]["Payload"]);
+            Assert.AreEqual(noteCreationDate, notes[0]["CreateDate"]);
+            Assert.AreEqual(notesID, notes[0]["NoteID"]);
+            Assert.AreEqual("true", notes[0]["IsDeleted"]);
+
+        }
+
+        [TestMethod()]
+        public void SoftDeleteServiceNotesTest()
+        {
+            //Assign
+            var mockFS = new Mock<IFileService>();
+            var dbm = new DatabaseManager(mockFS.Object);
+            var ua = new UserAuth();
+            var dbrs = new DBRelationshipService(dbm, ua);
+            var ndbs = new NotesDBService(dbm, dbrs);
+
+            var serviceID = "testID";
+            var notesID = "testNoteID";
+
+            var services_notes_relationshipsFilepath = "services_notes.json";
+            var initialServicesNotesRelationships = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"serviceID", serviceID},
+                    {"noteID", notesID}
+                }
+            };
+
+            var noteTitle = "Test Note";
+            var notePayload = "This is a test note.";
+            var noteCreationDate = DateTime.Now.ToString();
+
+            var notesDBFilepath = "notes.json";
+            var initialNotesDB = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"Title", noteTitle},
+                    {"Payload", notePayload},
+                    {"CreateDate", noteCreationDate},
+                    {"NoteID", notesID}
+                }
+            };
+
+            mockFS.Setup(fs => fs.ReadAllText(services_notes_relationshipsFilepath)).Returns(JsonSerializer.Serialize(initialServicesNotesRelationships));
+            mockFS.Setup(fs => fs.Exists(services_notes_relationshipsFilepath)).Returns(true);
+
+            mockFS.Setup(fs => fs.ReadAllText(notesDBFilepath)).Returns(JsonSerializer.Serialize(initialNotesDB));
+            mockFS.Setup(fs => fs.Exists(notesDBFilepath)).Returns(true);
+
+            string retrievedServicesNotesRelationships = null;
+            mockFS.Setup(fs => fs.WriteAllText(services_notes_relationshipsFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedServicesNotesRelationships = data);
+
+            string retrievedNotesDB = null;
+            mockFS.Setup(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedNotesDB = data);
+
+            //Act
+            ndbs.SoftDeleteServiceNotes(notesID);
+
+            //Assert
+            Assert.IsNotNull(retrievedServicesNotesRelationships);
+            //deserialize the json string and check if the relationship was soft deleted
+            var relationships = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedServicesNotesRelationships);
+            Assert.AreEqual(1, relationships.Count);
+            Assert.AreEqual(serviceID, relationships[0]["serviceID"]);
+            Assert.AreEqual(notesID, relationships[0]["noteID"]);
+            Assert.AreEqual("true", relationships[0]["IsDeleted"]);
+
+            Assert.IsNotNull(retrievedNotesDB);
+            //deserialize the json string and check if the note was soft deleted
+            var notes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedNotesDB);
+            Assert.AreEqual(1, notes.Count);
+            Assert.AreEqual(noteTitle, notes[0]["Title"]);
+            Assert.AreEqual(notePayload, notes[0]["Payload"]);
+            Assert.AreEqual(noteCreationDate, notes[0]["CreateDate"]);
+            Assert.AreEqual(notesID, notes[0]["NoteID"]);
+            Assert.AreEqual("true", notes[0]["IsDeleted"]);
+
+        }
+
+        [TestMethod()]
+        public void SoftDeleteInvoiceNotesTest()
+        {
+            //Assign
+            var mockFS = new Mock<IFileService>();
+            var dbm = new DatabaseManager(mockFS.Object);
+            var ua = new UserAuth();
+            var dbrs = new DBRelationshipService(dbm, ua);
+            var ndbs = new NotesDBService(dbm, dbrs);
+
+            var invoiceID = "testID";
+            var notesID = "testNoteID";
+
+            var invoices_notes_relationshipsFilepath = "invoices_notes.json";
+            var initialInvoicesNotesRelationships = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"invoiceID", invoiceID},
+                    {"noteID", notesID}
+                }
+            };
+
+            var noteTitle = "Test Note";
+            var notePayload = "This is a test note.";
+            var noteCreationDate = DateTime.Now.ToString();
+
+            var notesDBFilepath = "notes.json";
+            var initialNotesDB = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"Title", noteTitle},
+                    {"Payload", notePayload},
+                    {"CreateDate", noteCreationDate},
+                    {"NoteID", notesID}
+                }
+            };
+
+            mockFS.Setup(fs => fs.ReadAllText(invoices_notes_relationshipsFilepath)).Returns(JsonSerializer.Serialize(initialInvoicesNotesRelationships));
+            mockFS.Setup(fs => fs.Exists(invoices_notes_relationshipsFilepath)).Returns(true);
+
+            mockFS.Setup(fs => fs.ReadAllText(notesDBFilepath)).Returns(JsonSerializer.Serialize(initialNotesDB));
+            mockFS.Setup(fs => fs.Exists(notesDBFilepath)).Returns(true);
+
+            string retrievedInvoicesNotesRelationships = null;
+            mockFS.Setup(fs => fs.WriteAllText(invoices_notes_relationshipsFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedInvoicesNotesRelationships = data);
+
+            string retrievedNotesDB = null;
+            mockFS.Setup(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedNotesDB = data);
+
+            //Act
+            ndbs.SoftDeleteInvoiceNotes(notesID);
+
+            //Assert
+            Assert.IsNotNull(retrievedInvoicesNotesRelationships);
+            //deserialize the json string and check if the relationship was soft deleted
+            var relationships = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedInvoicesNotesRelationships);
+            Assert.AreEqual(1, relationships.Count);
+            Assert.AreEqual(invoiceID, relationships[0]["invoiceID"]);
+            Assert.AreEqual(notesID, relationships[0]["noteID"]);
+            Assert.AreEqual("true", relationships[0]["IsDeleted"]);
+
+            Assert.IsNotNull(retrievedNotesDB);
+            //deserialize the json string and check if the note was soft deleted
+            var notes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedNotesDB);
+            Assert.AreEqual(1, notes.Count);
+            Assert.AreEqual(noteTitle, notes[0]["Title"]);
+            Assert.AreEqual(notePayload, notes[0]["Payload"]);
+            Assert.AreEqual(noteCreationDate, notes[0]["CreateDate"]);
+            Assert.AreEqual(notesID, notes[0]["NoteID"]);
+            Assert.AreEqual("true", notes[0]["IsDeleted"]);
+
+        }
+
+        [TestMethod()]
+        public void SoftDeleteNoteTest()
+        {
+            //Arrange
+            var mockFS = new Mock<IFileService>();
+            var dbm = new DatabaseManager(mockFS.Object);
+            var ua = new UserAuth();
+            var dbrs = new DBRelationshipService(dbm, ua);
+            var ndbs = new NotesDBService(dbm, dbrs);
+
+            var primaryID = "primaryID";
+            var notesID = "testNoteID";
 
             var relationshipPath = "testRelationshipPath.json";
             var initialRelationshipDB = new List<Dictionary<string, string>>
@@ -766,34 +1033,43 @@ namespace Groomy.Notes.Tests
                 }
             };
 
-            mockFS.Setup(fs => fs.ReadAllText(notesDBFilepath)).Returns(JsonSerializer.Serialize(initialNotesDB));
-            mockFS.Setup(fs => fs.Exists(notesDBFilepath)).Returns(true);
+            var noteTitle = "Test Note";
+            var notePayload = "This is a test note.";
+            var noteCreationDate = DateTime.Now.ToString();
+
+            var notesDBFilepath = "notes.json";
+            var initialNotesDB = new List<Dictionary<string, string>>
+            {
+                new Dictionary<string, string>
+                {
+                    {"Title", noteTitle},
+                    {"Payload", notePayload},
+                    {"CreateDate", noteCreationDate},
+                    {"NoteID", notesID}
+                }
+            };
 
             mockFS.Setup(fs => fs.ReadAllText(relationshipPath)).Returns(JsonSerializer.Serialize(initialRelationshipDB));
             mockFS.Setup(fs => fs.Exists(relationshipPath)).Returns(true);
 
-            string retrievedNotesDB = null;
-            mockFS.Setup(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()))
-                .Callback<string, string>((path, data) => retrievedNotesDB = data);
+            mockFS.Setup(fs => fs.ReadAllText(notesDBFilepath)).Returns(JsonSerializer.Serialize(initialNotesDB));
+            mockFS.Setup(fs => fs.Exists(notesDBFilepath)).Returns(true);
 
             string retrievedRelationship = null;
             mockFS.Setup(fs => fs.WriteAllText(relationshipPath, It.IsAny<string>()))
                 .Callback<string, string>((path, data) => retrievedRelationship = data);
 
+            string retrievedNotesDB = null;
+            mockFS.Setup(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()))
+                .Callback<string, string>((path, data) => retrievedNotesDB = data);
+
             //Act
             ndbs.SoftDeleteNote(notesID, relationshipPath);
 
-            //Assert
-            Assert.IsNotNull(retrievedNotesDB);
-            //deserialize the json string and check if the note was soft deleted
-            var notes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedNotesDB);
-            Assert.AreEqual(1, notes.Count);
-            Assert.AreEqual(noteTitle, notes[0]["Title"]);
-            Assert.AreEqual(notePayload, notes[0]["Payload"]);
-            Assert.AreEqual(noteCreationDate, notes[0]["CreateDate"]);
-            Assert.AreEqual(notesID, notes[0]["NoteID"]);
-            Assert.AreEqual("true", notes[0]["IsDeleted"]);
+            mockFS.Verify(fs => fs.WriteAllText(relationshipPath, It.IsAny<string>()), Times.Once);
+            mockFS.Verify(fs => fs.WriteAllText(notesDBFilepath, It.IsAny<string>()), Times.Once);
 
+            //Assert
             Assert.IsNotNull(retrievedRelationship);
             //deserialize the json string and check if the relationship was soft deleted
             var relationships = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedRelationship);
@@ -801,6 +1077,16 @@ namespace Groomy.Notes.Tests
             Assert.AreEqual(primaryID, relationships[0]["PrimaryID"]);
             Assert.AreEqual(notesID, relationships[0]["ForeignID"]);
             Assert.AreEqual("true", relationships[0]["IsDeleted"]);
+
+            Assert.IsNotNull(retrievedNotesDB);
+            //deserialize the json string and check if the note was soft deleted
+            var notes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(retrievedNotesDB);
+            Assert.AreEqual(1, notes.Count);
+            Assert.AreEqual(noteTitle, notes[0]["Title"]);
+            Assert.AreEqual(notePayload, notes[0]["Payload"]);
+            Assert.AreEqual(noteCreationDate, notes[0]["CreateDate"]);
+
+
         }
     }
 }
